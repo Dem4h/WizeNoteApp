40ae0bc0d11a7eb7320946bfcd6c74fd3d918278
demah <demah.contact@gmail.com>
2025-05-25 18:24:58 +0200

    added auth with token inside cookie

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,6 +3,43 @@ import { credentials } from "../lib/_credentials.svelte.ts";
 //import { CapacitorCookies } from "@capacitor/core";
 
 export const load = async ({ cookies }) => {
+  if (cookies.get("login") == undefined) {
+    console.log("No Token");
+    const handle = await pronote.createSessionHandle();
+    const login = await pronote.loginQrCode(handle, {
+      deviceUUID: credentials.deviceUUID,
+      pin: credentials.pin,
+      qr: JSON.parse(credentials.qr),
+    });
+    const account = await pronote.account(handle);
+    console.log(account);
+    console.log(login);
+
+    cookies.set("login", JSON.stringify(login), { path: "/" });
+    return {
+      login,
+    };
+  } else {
+    console.log("Token");
+    const prev_login = await JSON.parse(cookies.get("login"));
+    console.log(prev_login);
+    const handle = pronote.createSessionHandle();
+    const login = await pronote.loginToken(handle, {
+      deviceUUID: credentials.deviceUUID,
+      kind: prev_login.kind,
+      url: prev_login.url,
+      username: prev_login.username,
+      token: prev_login.token,
+    });
+
+    const account = await pronote.account(handle);
+    console.log(account);
+    return {
+      login,
+    };
+  }
+  /*
+
   console.log("------ PIN:");
   const handle = pronote.createSessionHandle();
   const login = await pronote.loginQrCode(handle, {
@@ -31,11 +68,11 @@ export const load = async ({ cookies }) => {
   console.info("Username:", next_login.username);
   console.info("Next-Time Token:", next_login.token);
   cookies.set("login", JSON.stringify(next_login), { path: "/" });
-
-  return {
-    login,
-  };
+  console.log(cookies.get("lugin"));
+  */
 };
-//en gros export token
-//si token existe bah on permet a l'utilisateur de se co dessus (deja fait juste load les elements)
-//sinon bah qr code hein
+
+// check login == udefined :
+// true -> go for qr and pin auth and add cookie
+// false -> login = token : , username :, kind:,url:,navigatorIdentifier:,
+// auth with token and add cookie
